fdb1924b67f1a89ada51fbb977f2b56ebdd942bd
KAYCK RENAN <167095452+CamisUnirv@users.noreply.github.com>
2026-06-26 11:38:37 -0300

    feat: adiciona referências dos elementos (#3)
    Cria as constantes para acessar o campo de texto, botão de cadastro e lista de tarefas.

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("Projeto iniciado");
+const inputTarefa = document.getElementById("inputTarefa");
+const btnAdicionar = document.getElementById("btnAdicionar");
+const listaTarefas = document.getElementById("listaTarefas");
